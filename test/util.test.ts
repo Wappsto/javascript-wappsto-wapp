4039e379b9edac0323039ae5b1dac4aa40f6a6bf
Andreas Bomholtz <andreas@seluxit.com>
2021-11-10 11:34:58 +0100

    Only build on ubuntu

--- test/util.test.ts
+++ test/util.test.ts
@@ -2,7 +2,12 @@ import { isUUID } from '../src/util/uuid';
 
 describe('util', () => {
     it('can validate an UUID', () => {
-        expect(isUUID('asd')).toEqual(false);
         expect(isUUID('b8546b68-548a-420b-8882-b96dbaa1edae')).toEqual(true);
+    });
+
+    it('can see that it is not an UUID', () => {
+        expect(isUUID('asd')).toEqual(false);
+        expect(isUUID('b8546b68-548a-420b-8882-b96dbaa1eda')).toEqual(false);
+        expect(isUUID('b8546b68-548a-420b-8882-b96dbaa1edag')).toEqual(false);
     });
 });
