7838418ea670ede1b42c4c5086bcadc11ad4f651
Andreas Bomholtz <andreas@seluxit.com>
2021-11-10 11:02:05 +0100

    Added user model

---
+++ src/models/user.ts
@@ -0,0 +1,36 @@
+import { Model } from './model';
+
+export class User extends Model {
+    static endpoint = '/2.1/user';
+
+    first_name?: string;
+    last_name?: string;
+    email?: string;
+    phone?: string;
+    name?: string;
+    nickname?: string;
+    language?: string;
+    friend?: boolean;
+    blocked?: boolean;
+    provider?: any[];
+    public?: any[];
+    verified_email?: boolean;
+
+    url(): string {
+        return User.endpoint;
+    }
+
+    attributes(): string[] {
+        return ['first_name','lat_name','email','phone','name','nickname','language'];
+    }
+
+    public static fetch = async () => {
+        let data: any = await Model.fetch(User.endpoint + '/me');
+        return User.fromJSON(data);
+    };
+
+    static fromJSON(json: any): User {
+        let user = Object.create(User.prototype);
+        return Object.assign(user, json);
+    }
+}
