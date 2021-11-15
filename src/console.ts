8e7db9a78917db4d4010193fbbd0a331815ab97e
Andreas Bomholtz <andreas@seluxit.com>
2021-11-15 08:54:06 +0100

    Removed unused code in console

--- src/console.ts
+++ src/console.ts
@@ -2,27 +2,13 @@ import wappsto from './util/http_wrapper';
 
 const defaultConsole = Object.assign({}, console);
 
-function replacer(_: string, value: any): any {
-    if (value === undefined) {
-        return 'undefined';
-    } else if (value === null) {
-        return 'null';
-    } else if (typeof value === 'function') {
-        return 'function';
-    }
-    return value;
-}
-
 function sendExtsync(key: string, ...args: any[]): any {
     const time = new Date().toISOString();
-    const data = JSON.stringify(
-        {
-            key,
-            args,
-            time,
-        },
-        replacer
-    );
+    const data = JSON.stringify({
+        key,
+        args,
+        time,
+    });
 
     const req = wappsto
         .post('/2.1/extsync/wappsto/editor/console', data)
