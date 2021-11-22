3fb4e420c65b26e7263fc2ad370492a045a977d8
Andreas Bomholtz <andreas@seluxit.com>
2021-11-22 14:47:47 +0100

    Added notification and stream to model index

--- src/models/index.ts
+++ src/models/index.ts
@@ -4,3 +4,5 @@ export { Value } from './value';
 export { State } from './state';
 export { User } from './user';
 export { Data } from './data';
+export { Stream } from './stream';
+export { Notification } from './notification';
