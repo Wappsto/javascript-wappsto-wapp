3f81d0b81369cb2421bd7e46ab0c1c79d109bef1
Andreas Bomholtz <andreas@seluxit.com>
2021-11-22 20:24:36 +0100

    Linting

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 
-export {verbose, debug} from './util/settings';
+export { verbose, debug } from './util/settings';
 export * from './console';
 export * from './models';
 export { default as request } from './util/http_wrapper';
