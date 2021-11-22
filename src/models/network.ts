04c10c33f7516e8034a6ace568bcd3a9b2603f4c
Andreas Bomholtz <andreas@seluxit.com>
2021-11-22 20:26:53 +0100

    Added more helper functions to network

--- src/models/network.ts
+++ src/models/network.ts
@@ -1,10 +1,13 @@
-import * as _ from 'lodash';
+import { Type } from 'class-transformer';
+import { PermissionModel } from './model.permission';
 import { Model } from './model';
 import { Device } from './device';
+import { Value } from './value';
 
-export class Network extends Model {
+export class Network extends PermissionModel {
     static endpoint = '/2.0/network';
     name?: string;
+    @Type(() => Device)
     device: Device[] = [];
 
     get devices() {
@@ -12,35 +15,60 @@ export class Network extends Model {
     }
 
     constructor(name?: string) {
-        super();
+        super('network');
         this.name = name;
-    }
-
-    url(): string {
-        return Network.endpoint;
     }
 
     attributes(): string[] {
         return ['name', 'device'];
     }
 
+    public findDeviceByName(name: string): Device[] {
+        return this.device.filter((dev) => dev.name === name);
+    }
+
+    public findValueByName(name: string): Value[] {
+        let values: Value[] = [];
+        this.device.forEach((dev) => {
+            values.concat(dev.findValueByName(name));
+        });
+        return values;
+    }
+
     static getUrl(): string {
         return Network.endpoint;
     }
+
+    static findById = async (id: string) => {
+        let data: any = await Model.fetch(`${Network.endpoint}/${id}`, {
+            expand: 3,
+        });
+        return Network.fromArray(data)[0];
+    };
+
+    static findByName = async (
+        name: string,
+        quantity: number = 1,
+        usage: string = ''
+    ) => {
+        if (usage === '') {
+            usage = `Find network ${quantity} with name ${name}`;
+        }
+
+        let data = await PermissionModel.request(
+            `${Network.endpoint}`,
+            quantity,
+            usage,
+            {
+                this_name: name,
+                expand: 3,
+            }
+        );
+        return Network.fromArray(data);
+    };
 
     static fetch = async () => {
         let data: any[] = await Model.fetch(Network.endpoint, { expand: 3 });
         return Network.fromArray(data);
     };
-
-    static fromJSON(json: any): Network {
-        let network = new Network();
-        let devices: Device[] = [];
-        json.device?.forEach((dev: any) => {
-            devices.push(Device.fromJSON(dev));
-        });
-        return Object.assign(network, _.omit(json, ['device']), {
-            device: devices,
-        });
-    }
 }
