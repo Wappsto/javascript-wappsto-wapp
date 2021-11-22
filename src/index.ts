b2d67cc9276d08437e56f232c7e575176dd5f01c
Andreas Bomholtz <andreas@seluxit.com>
2021-11-22 14:46:04 +0100

    Moved settings functions to settings file

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
 import 'reflect-metadata';
-import settings from './util/settings';
 
+export {verbose, debug} from './util/settings';
 export * from './console';
 export * from './models';
 export { default as request } from './util/http_wrapper';
-
-export function verbose(mode: boolean): void {
-    settings.verbose = mode;
-}
-
-export function debug(mode: boolean): void {
-    settings.debug = mode;
-}
--- src/util/settings.ts
+++ src/util/settings.ts
@@ -6,3 +6,11 @@ class Settings {
 let settings = new Settings();
 
 export default settings;
+
+export function verbose(mode: boolean): void {
+    settings.verbose = mode;
+}
+
+export function debug(mode: boolean): void {
+    settings.debug = mode;
+}
