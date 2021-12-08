93dd9d76217e21f38a8814a0abe5556a0a78e9ed
Andreas Bomholtz <andreas@seluxit.com>
2021-12-08 15:56:07 +0100

    Disabled config test

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,3 @@
-import wappsto from './util/http_wrapper';
 import { Data } from './models/data';
 
 export class Config {
@@ -11,83 +10,31 @@ export class Config {
 
     constructor(name: string) {
         this.name = name;
+        this.data = new Data();
+/*
+        Data.findById(name + '_config').then((data) => {
 
-        this.data = new Data(undefined, name + '_config');
-    }
-
-    get_id = async () => {
-        if (this.id !== '') {
-            return this.id;
-        }
-        if (this.id_fetched) {
-            return this.id;
-        }
-        this.id_fetched = true;
-        await wappsto
-            .get('/2.1/user/me')
-            .then((response) => {
-                this.id = response.data.meta.id + '_' + this.name;
-                if (this.data.data_meta) {
-                    this.data.data_meta.id = this.id;
-                }
-                this.data.meta.redirect = this.id;
-            })
-            .catch(() => {
-                this.id_fetched = false;
-            });
-        return this.id;
-    };
-
-    get_data = async () => {
-        if (this.data.meta.id) {
-            return this.data;
-        }
-        if (this.data_fetched || this.id !== '') {
-            return this.data;
-        }
-        this.data_fetched = true;
-        await this.data.load();
-        return this.data;
-    };
-
-    create_data = async () => {
-        if (!this.id) {
-            await this.get_id();
-        }
-
-        await this.data.create();
-        this.created = true;
-
-        return this.data;
-    };
-
-    patch = async (body: any) => {
-        if (this.created) {
-            var self = this;
-            wappsto.patch('/2.0/data/' + this.id, body).then((response) => {
-                self.data = response.data;
-            });
-        }
-    };
-
-    patch_data = async (path: string, value: string) => {
-        let arrPath = path.split('.');
-        var data = this.data.data;
-        arrPath.forEach(function (p) {
-            if (p !== arrPath[arrPath.length - 1]) {
-                if (!data.data[p]) {
-                    data.data[p] = {};
-                }
-                data = data[p];
-            } else {
-                data[p] = value;
-            }
         });
-        if (this.created) {
-            wappsto.put('/2.0/data/' + this.id, this.data);
-        } else {
-            this.create_data();
-        }
+        this.data = new Data(name + '_config', 'wapp_config');
+        this.data.refresh().then((response) => {
+            if (response.data === {}) {
+                this.data.create();
+            }
+        });*/
+    }
+/*
+    public set = async (name: string, item: any): Promise<void> => {
+        this.data.set(name, item);
+        this.data.update();
+    };
+
+    public get = async (name: string): Promise<any> => {
+        return new Promise<any>(async (response) => {
+            if (this.data.meta.id) {
+                await this.data.refresh();
+            }
+            resolve(this.data.get(name));
+        });
     };
 
     reset_data(): void {
@@ -97,5 +44,5 @@ export class Config {
         } else {
             this.create_data();
         }
-    }
+    }*/
 }
--- test/config.test.ts
+++ test/config.test.ts
@@ -5,4 +5,11 @@ describe('config', () => {
         let c = new Config('test');
         expect(c.name).toEqual('test');
     });
+/*
+    it('can add a new item', async () => {
+        let c = new Config('test');
+        c.set('key', 'item');
+        let res = c.get('key');
+        expect(res).toBe('item');
+    });*/
 });
