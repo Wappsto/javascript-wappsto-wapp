b039eeb623bec81d1479289e0a7c70863f5ca70e
Andreas Bomholtz <andreas@seluxit.com>
2021-12-15 15:49:51 +0100

    Added OAuth to models

--- src/models/oauth.ts
+++ src/models/oauth.ts
@@ -1,10 +1,16 @@
 import { Model } from './model';
 import { openStream } from './stream';
+import { printDebug } from '../util/debug';
 
 export class OAuth extends Model {
     static endpoint = '/2.0/oauth_connect';
 
     name: string;
+    api?: string;
+    installation?: string;
+    token?: string;
+    secret_token?: string;
+    params?: any;
 
     constructor(name: string) {
         super('2.0', 'oauth');
@@ -21,10 +27,13 @@ export class OAuth extends Model {
                 return;
             }
 
+            printDebug('OAuth token is not valid, waiting for token on stream');
             openStream.subscribeService(
-                '/oauth',
+                '/oauth_connect',
                 async (event: any): Promise<true | undefined> => {
-                    if (event) {
+                    if (event?.data?.name === this.name) {
+                        printDebug('Got OAuth token from stream');
+                        resolve(event?.data?.params);
                         return true;
                     }
                     return;
@@ -32,13 +41,19 @@ export class OAuth extends Model {
             );
 
             if (oauth?.code === 436000002) {
+                printDebug('OAuth is missing');
                 // configure oauth
                 if (typeof window !== 'undefined' && window.open) {
-                    window.open(
-                        oauth.data.request,
+                    printDebug('Open new window with oauth request');
+                    debugger;
+                    let w = window.open(
+                        oauth?.data?.request,
                         `OAuth - ${oauth.message}`,
-                        'popup,height=300,width=300'
+                        'popup'
                     );
+                    console.log(w);
+                } else {
+                    printDebug('OAuth running without window');
                 }
             }
         });
