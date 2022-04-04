b79d1fb81ee196c5a504dc99c4a2224d79011581
Andreas Bomholtz <andreas@seluxit.com>
2022-04-04 15:21:12 +0200

    Added event log model

---
+++ src/models/eventlog.ts
@@ -0,0 +1,19 @@
+import { Model } from './model';
+import { IEventLog, EventLogLevel } from '../util/interfaces';
+
+export class EventLog extends Model implements IEventLog {
+    static endpoint = '/2.0/eventlog';
+    message = '';
+    level: EventLogLevel = 'info';
+    info?: Record<string, any>;
+    type?: string;
+    timestamp?: Date;
+
+    constructor() {
+        super('eventlog');
+    }
+
+    attributes(): string[] {
+        return ['message', 'level', 'into', 'type'];
+    }
+}
