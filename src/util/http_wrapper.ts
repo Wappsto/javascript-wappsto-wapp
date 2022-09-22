7f7dfeead3c72b5e6f680e0f5390e9564b6112b1
Andreas Bomholtz <andreas@seluxit.com>
2022-09-22 12:18:19 +0200

    Catch fatal errors and exit with 11

--- src/util/http_wrapper.ts
+++ src/util/http_wrapper.ts
@@ -2,13 +2,60 @@ import axios from 'axios';
 import { session, baseUrl } from '../session';
 import { printError, printDebug } from './debug';
 
-export default axios.create({
+const axiosInstance = axios.create({
     baseURL: baseUrl,
     headers: {
         'X-Session': session,
         'Content-Type': 'application/json',
     },
 });
+
+type AxiosFun = (url: string, data?: any, config?: any) => Promise<any>;
+async function wrap(func: AxiosFun, url: string, data?: any, config?: any) {
+    try {
+        if (config === undefined) {
+            return await func(url, data);
+        } else {
+            return await func(url, data, config);
+        }
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            /* istanbul ignore next */
+            if (axios.isAxiosError(error)) {
+                /* istanbul ignore next */
+                if (error.response === undefined) {
+                    /* istanbul ignore next */
+                    process.stderr.write(
+                        `WAPPSTO FATAL ERROR: ${error.message}\n`
+                    );
+                    /* istanbul ignore next */
+                    process.exit(11);
+                }
+            }
+        }
+        throw error;
+    }
+}
+
+const wrapper = {
+    get: async (url: string, config?: any): Promise<any> => {
+        return await wrap(axiosInstance.get, url, config);
+    },
+    post: async (url: string, data?: any, config?: any): Promise<any> => {
+        return await wrap(axiosInstance.post, url, data, config);
+    },
+    patch: async (url: string, data?: any, config?: any): Promise<any> => {
+        return await wrap(axiosInstance.patch, url, data, config);
+    },
+    put: async (url: string, data?: any, config?: any): Promise<any> => {
+        return await wrap(axiosInstance.put, url, data, config);
+    },
+    delete: async (url: string, config?: any): Promise<any> => {
+        return await wrap(axiosInstance.delete, url, config);
+    },
+};
+
+export default wrapper;
 
 export function getErrorResponse(error: any): any {
     /* istanbul ignore next */
