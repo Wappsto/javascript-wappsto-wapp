12fe2272a03758fabf35c3e7fa871b52385f273e
Andreas Bomholtz <andreas@seluxit.com>
2023-11-03 09:55:42 +0100

    Removed full url in test.

--- test/stream.test.ts
+++ test/stream.test.ts
@@ -254,7 +254,7 @@ describe('stream', () => {
                 type: 'event',
             },
             event: 'update',
-            path: '/network/9a51cbd4-afb3-4628-9c8b-df64a0d729e9/device/c5fe846f-d6d8-413a-abb5-620519dd6b75/value/6c06b63e-39ec-44a5-866a-c081aafb6726/state/cda4d978-39e9-47bf-8497-9813b0f94973',
+            path: '/state/cda4d978-39e9-47bf-8497-9813b0f94973',
             data: {
                 data: 'data',
                 timestamp: 'timestamp',
@@ -266,7 +266,7 @@ describe('stream', () => {
                 type: 'event',
             },
             event: 'update',
-            path: '/network/9a51cbd4-afb3-4628-9c8b-df64a0d729e9/device/c5fe846f-d6d8-413a-abb5-620519dd6b75/value/6c06b63e-39ec-44a5-866a-c081aafb6726/state/2e91c9a5-1ca5-4a93-b4d5-74bd662e6d59',
+            path: '/state/2e91c9a5-1ca5-4a93-b4d5-74bd662e6d59',
             data: {
                 data: 'data',
                 timestamp: 'timestamp',
