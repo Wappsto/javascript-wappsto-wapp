b243f22b54ed12cbc0e89a50101d8d96c1e09c6b
Andreas Bomholtz <andreas@seluxit.com>
2024-08-08 16:08:58 +0200

    Added function to modelStore to just find a model.

--- src/util/modelStore.ts
+++ src/util/modelStore.ts
@@ -1,3 +1,4 @@
+import { Data } from '../models';
 import { IModel } from './types';
 
 type createCallback = (
@@ -16,12 +17,12 @@ export function addModel(model: IModel): IModel;
 export function addModel(model: undefined): undefined;
 export function addModel(model?: IModel) {
     if (model && model.id()) {
-        if (!models[model.id()]) {
-            models[model.id()] = model;
-        } else {
+        if (models[model.id()]) {
             models[model.id()].parse(
                 model as unknown as Record<string, unknown>
             );
+        } else {
+            models[model.id()] = model;
         }
         return models[model.id()];
     }
@@ -32,10 +33,11 @@ export async function getModel(
     type: string,
     id: string
 ): Promise<IModel | false | undefined> {
-    const model = models[id];
-    if (model !== undefined) {
+    const model = findModel(type, id);
+    if (model) {
         return model;
     }
+
     if (createModel !== undefined) {
         const m = await createModel(type, id);
         if (m) {
@@ -43,7 +45,32 @@ export async function getModel(
         }
         return m;
     }
+
     /* istanbul ignore next */
+    return undefined;
+}
+
+export function findModel(type: string, id: string): IModel | undefined {
+    const model = models[id];
+    if (model !== undefined) {
+        return model;
+    }
+
+    if (type === 'data') {
+        const data_model = Object.values(models).find((m: IModel) => {
+            if (
+                m.getType() === 'data' &&
+                (m as Data<Record<string, unknown>>).data_meta.id === id
+            ) {
+                return true;
+            }
+            return false;
+        });
+        if (data_model) {
+            return data_model;
+        }
+    }
+
     return undefined;
 }
 
